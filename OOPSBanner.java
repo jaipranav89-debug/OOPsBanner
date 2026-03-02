ac9cadf731af8d3725ceeadf4c6ebc63f55d5604
jaipranav89-debug <jaipranv89@gmail.com>
2026-03-02 09:26:08 +0530

    Using static function

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,5 +1,62 @@
-public class OOPSBanner{
-	public static void main(String[] args){
-		System.out.println("OOPS");
-	}
+public class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            buildLine(0),
+            buildLine(1),
+            buildLine(2),
+            buildLine(3),
+            buildLine(4),
+            buildLine(5),
+            buildLine(6)
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+
+    private static String buildLine(int index) {
+        return getO()[index] + "  " +
+               getO()[index] + "  " +
+               getP()[index] + "  " +
+               getS()[index];
+    }
+
+    public static String[] getO() {
+        return new String[]{
+            "  *****  ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "  *****  "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[]{
+            "******   ",
+            "**    ** ",
+            "**    ** ",
+            "******   ",
+            "**       ",
+            "**       ",
+            "**       "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[]{
+            " ******  ",
+            "**    ** ",
+            "**       ",
+            " ******  ",
+            "       **",
+            "**    ** ",
+            " ******  "
+        };
+    }
 }
